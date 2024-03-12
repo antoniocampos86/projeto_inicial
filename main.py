459ab0370be8cd068d0c34ef5d5528390be6dcfd
Antonio Campos <contato@antoniocampos.com.br>
2024-03-12 19:54:50 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Jornada de Dados")
+
+#variaveis
+idade = 37
+nome = "Antonio"
+
+#print
+print(idade)
+
+#soma
+
+nova_idade = idade + 1
+print(nova_idade)
+
+#lista
+lista = [1,2,3,4]
+print(lista)
+
+#metodos
+#lista_invertida = lista.re
